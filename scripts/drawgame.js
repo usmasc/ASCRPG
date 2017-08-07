60e77b53f68d9b0897ed849ef715c2d6f4a2ed06
Scottt <scott.geyer@eagles.usm.edu>
2017-08-07 02:19:50 -0500

    Update drawgame.js

--- scripts/drawgame.js
+++ scripts/drawgame.js
@@ -17,23 +17,23 @@ void draw() {
   ellipse(player.x,player.y-10,12+player.face*4,20);
   
   // arms
-  rect(player.x-14,player.y+4,4,18);
-  rect(player.x+10,player.y+4,4,18);
+  rect(player.x-10,player.y+4,4,8);
+  rect(player.x+6,player.y+4,4,8);
   
   // tshirt
   fill(player.tColor[0],player.tColor[1],player.tColor[2]);
-  rect(player.x-10,player.y,20,20);
-  rect(player.x-14,player.y,4,4);
-  rect(player.x+10,player.y,4,4);
+  rect(player.x-4,player.y,8,10);
+  rect(player.x-10,player.y,4,4);
+  rect(player.x+6,player.y,4,4);
   
   // hat
   rect(player.x-player.face-6, player.y-25, player.face*2+12, 5);
-  fill(0,0,0);
-  rect(player.x-player.face-6, player.y-21, 3, 2);
+  fill(0,0,100);
+  rect(player.x-player.face-6-3, player.y-21, 3, 2);
   
   // tshirt logo
   fill(255,255,255);
-  text(player.tshirt,player.x-3,player.y+13);
+  text(player.tshirt,player.x-3,player.y+5);
  
   // eye whites
   fill(255,255,255);
@@ -50,14 +50,14 @@ void draw() {
   
   //pants
   fill(0,100,0);
-  rect(player.x-10,player.y+20,20,10);
-  rect(player.x-10,player.y+30,5,10);
-  rect(player.x+5, player.y+30,5,10);
+  rect(player.x-4,player.y+10,8,4);
+  rect(player.x-4,player.y+14,3,4);
+  rect(player.x+2, player.y+14,3,4);
   
   //shoes
   fill(0,0,0);
-  rect(player.x-11,player.y+40,7,3);
-  rect(player.x+4,player.y+40,7,3);
+  rect(player.x-5,player.y+19,5,2);
+  rect(player.x+2,player.y+19,5,2);
   
   
 }
