c7a929f608ad622c5a443c671d13b58bada4dc58
Scottt <scott.geyer@eagles.usm.edu>
2017-08-07 01:20:31 -0500

    Update drawgame.js

--- scripts/drawgame.js
+++ scripts/drawgame.js
@@ -14,8 +14,12 @@ void draw() {
   
   fill(player.skin[0],player.skin[1],player.skin[2]);
   ellipse(player.x,player.y-10,12+player.face*4,20);
+  rect(player.x-14,player.y+4,4.6);
+  rect(player.x+10,player.y+4,4.6);
   fill(player.tColor[0],player.tColor[1],player.tColor[2]);
   rect(player.x-10,player.y,20,20);
+  rect(player.x-14,player.y,4.4);
+  rect(player.x+10,player.y,4.4);
   fill(255,255,255);
-  text(player.tshirt,player.x-5,player.y+10);
+  text(player.tshirt,player.x-5,player.y+15);
 }
