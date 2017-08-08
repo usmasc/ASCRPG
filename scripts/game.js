9c1a40624587b8de4ac797d5e848bc34b83b4ebd
Scottt <scott.geyer@eagles.usm.edu>
2017-08-08 18:48:53 -0500

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,16 @@ var text = document.getElementById('textArea');
 
          // 1     2   3   4   5 6   7   8     9  10   11  12  13  14  15 16 17  18  19 20  21  22   23 24   25  26 27   28 
 
+function arrows() {
+   thingie = '<button onclick="up()">⇑</button><br>';
+    thingie += '<button onclick="left()">⇐</button> ';
+    thingie += '<button onclick="right()">⇒</button><br>';
+    thingie += '<button onclick="down()">⇒</button>';
+    sumbit.innerHTML = thingie;
+
+
 
 function start() {
   text.innerHTML = 'In the year 201X ...';
+  arrows();
 }
