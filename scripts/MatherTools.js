867793f0363930d06a497613691e9e4f529e24e2
Scottt <scott.geyer@eagles.usm.edu>
2017-08-08 16:07:45 -0500

    Create MatherTools.js

---
+++ scripts/MatherTools.js
@@ -0,0 +1,41 @@
+// map objects  
+  
+  var brk = {
+    "canPass":false,
+    "draw": 'brick');
+  };
+  
+    var rug = {
+    "canPass":true,
+    "draw": 'rug');
+  };
+  
+  var grs = {
+  "canPass":true,
+  "draw": 'grass'
+  }
+  
+  var smp = {
+  'canPass':true,
+  'draw':'swamp'
+  }
+  
+  var snd = {
+  'canPass':true,
+  'draw':'sand'
+  }
+  
+  var pth = {
+  'canPass':true,
+  'draw':'path'
+  }
+  
+  var flr = {
+  'canPass':true,
+  'draw':'floor'
+  }
+  
+  var wtr = {
+  'canPass':false,
+  'draw':'water'
+  }
