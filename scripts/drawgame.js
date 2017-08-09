0d8a1d2415af98838285150d973f971a9dbd68a6
Scottt <scott.geyer@eagles.usm.edu>
2017-08-08 20:58:17 -0500

    Update drawgame.js

--- scripts/drawgame.js
+++ scripts/drawgame.js
@@ -8,67 +8,7 @@ void setup() {
   //noLoop();
 }
 
-var text = document.getElementById('textArea');
-var sumbit = document.getElementById('submitButtonArea');
 
-
-var brk = {
-  "canPass":false,
-  "draw": 'brick'
-};
-
-var rug = {
-  "canPass":true,
-  "draw": 'rug'
-};
-
-var grs = {
-  "canPass":true,
-  "draw": 'grass'
-};
-
-var smp = {
-  'canPass':true,
-  'draw':'swamp'
-};
-
-var snd = {
-  'canPass':true,
-  'draw':'sand'
-};
-
-var pth = {
-  'canPass':true,
-  'draw':'path'
-};
-
-var flr = {
-  'canPass':true,
-  'draw':'floor'
-};
-
-var wtr = {
-  'canPass':false,
-  'draw':'water'
-};
-  
-var map = [[wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr],
-           [wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
-           [wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
-           [wtr,grs,brk,brk,brk,brk,brk,brk,grs,grs,grs,brk,brk,brk,brk,brk,brk,grs,grs,brk,brk,brk,brk,brk,brk,grs,grs,wtr],
-           [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
-           [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
-           [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
-           [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
-           [wtr,grs,brk,brk,flr,brk,brk,brk,grs,grs,grs,brk,brk,brk,rug,brk,brk,grs,grs,brk,brk,brk,brk,flr,brk,grs,grs,wtr],
-           [wtr,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,wtr],
-           [wtr,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,wtr],
-           [wtr,grs,grs,grs,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,grs,grs,grs,wtr],
-           [wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,pth,pth,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
-           [wtr,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,wtr],
-           [wtr,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,wtr],
-           [wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr],
-           [wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr]];
 
 void draw() {
   
@@ -360,42 +300,7 @@ void draw_bg(bgo, x,y) {
   }
 }
 
-void up() {
-   if (map[player.mapY-1][player.mapX].canPass){
-     player.mapY--;
-     text.innerHTML = 'Going up.';
-   } else {
-     text.innerHTML = 'None shall pass ' + map[player.mapY-1][player.mapX].draw;
-   }
-}
 
-void down() {
-   if (map[player.mapY+1][player.mapX].canPass){
-     player.mapY++;
-     text.innerHTML = 'Going dowm.';
-   } else {
-     text.innerHTML = 'None shall pass ' + map[player.mapY+1][player.mapX].draw;
-   }
-}
-
-
-void left() {
-   if (map[player.mapY][player.mapX-1].canPass){
-     player.mapX--;
-     text.innerHTML = 'Going left.';
-   } else {
-     text.innerHTML = 'None shall pass ' + map[player.mapY][player.mapX-1].draw;
-   }
-}
-
-void right() {
-   if (map[player.mapY][player.mapX+1].canPass){
-     text.innerHTML = 'Going right.';
-     player.mapX++;
-   } else {
-     text.innerHTML = 'None shall pass ' + map[player.mapY][player.mapX+1].draw;
-   }
-}
 
   
   
