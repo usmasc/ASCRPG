9cf2fdfeb8ad8e21cf01db60ee18416f49e433c2
Scottt <scott.geyer@eagles.usm.edu>
2017-08-09 18:13:23 -0500

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -12,6 +12,7 @@ var rug = {
   "canPass": true,
   "draw": 'rug',
   'canCheck': true,
+  'canTalk': false,
    check() {
     var thingie = 'No problems found here. While you are tempted, you refuse to sweep your problems under the rug.';
     text.innerHTML = thingie;
@@ -22,6 +23,7 @@ var grs = {
   "canPass": true,
   "draw": 'grass',
   'canCheck': true,
+  'canTalk': false,
   check() {
     var thingie = 'You could have sworn that the grass looked greener from further away. However, after closer analysis';
     thingie += ' the grass in the distance looks greener now.';
@@ -32,39 +34,61 @@ var grs = {
 var smp = {
   'canPass':true,
   'draw':'swamp',
-  'canCheck': false
+  'canCheck': false,
+  'canTalk': false
 };
 
 var snd = {
   'canPass':true,
   'draw':'sand',
-  'canCheck': false
+  'canCheck': false,
+  'canTalk': false
 };
 
 var pth = {
   'canPass':true,
   'draw':'path',
-  'canCheck': false
+  'canCheck': false,
+  'canTalk': false
 };
 
 var flr = {
   'canPass':true,
   'draw':'floor',
+   'canTalk': false,
   'canCheck': false
 };
 
 var wtr = {
   'canPass':false,
   'draw':'water',
+  'canTalk': false,
   'canCheck': false
 };
+
+var Frd = {
+  'name': 'Fred',
+  'canPass': false,
+  'draw': 'floor',
+  'canTalk': true,
+  'canCheck': true,
+  check() {
+    var thingie = "Fred: Hey, watch where you're touching";
+    text.innerHTML = thingie;
+  },
+  talk() {
+    var thingie = "Fred: You found me! What? You never seen an invisible person before?";
+    text.innerHTML = thingie;
+  }
+};
+  
 
 var map = [[wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr],
            [wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
            [wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
            [wtr,grs,brk,brk,brk,brk,brk,brk,grs,grs,grs,brk,brk,brk,brk,brk,brk,grs,grs,brk,brk,brk,brk,brk,brk,grs,grs,wtr],
            [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
-           [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
+           [wtr,grs,brk,flr,Frd,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
            [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
            [wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
            [wtr,grs,brk,brk,flr,brk,brk,brk,grs,grs,grs,brk,brk,brk,rug,brk,brk,grs,grs,brk,brk,brk,brk,flr,brk,grs,grs,wtr],
@@ -82,9 +106,9 @@ var map = [[wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,
 
 function up() {
    if (map[player.mapY-1][player.mapX].canPass){
-
+     player.facing = 'North';
      
-     text.innerHTML = 'Going up.';
+     text.innerHTML = 'facing '+player.facing;
      if (map[player.mapY-1][player.mapX] == grs){
       text.innerHTML = 'Stay off the grass';
       }
@@ -92,14 +116,14 @@ function up() {
    } else {
      text.innerHTML = 'None shall pass ' + map[player.mapY-1][player.mapX].draw;
    }
-     player.facing = 'North';
+
 }
 
 function down() {
    if (map[player.mapY+1][player.mapX].canPass){
 
-     
-     text.innerHTML = 'y-=1.';
+     player.facing = 'South';
+     text.innerHTML = 'facing '+player.facing;
      if (map[player.mapY+1][player.mapX] == grs){
         text.innerHTML =  'Stay off the grass';
       }
@@ -108,14 +132,14 @@ function down() {
    } else {
      text.innerHTML = 'None shall pass ' + map[player.mapY+1][player.mapX].draw;
    }
-     player.facing = 'South';
+
 }
 
 
 function left() {
    if (map[player.mapY][player.mapX-1].canPass){
-     
-     text.innerHTML = 'Going left.';
+     player.facing = 'West';    
+     text.innerHTML = 'facing '+player.facing;
      if (map[player.mapY][player.mapX-1] == grs){
         text.innerHTML =  'Stay off the grass';
      }
@@ -123,14 +147,13 @@ function left() {
    } else {
      text.innerHTML = 'None shall pass ' + map[player.mapY][player.mapX-1].draw;
    }
-       player.facing = 'West';
+
 }
 
 function right() {
    if (map[player.mapY][player.mapX+1].canPass){
-     text.innerHTML = 'Going right.';
-
-     
+     player.facing = 'East';
+     text.innerHTML = 'facing '+player.facing;
      if (map[player.mapY][player.mapX+1] == grs){
        text.innerHTML =  'Stay off the grass';
      }
@@ -138,7 +161,7 @@ function right() {
    } else {
      text.innerHTML = 'None shall pass ' + map[player.mapY][player.mapX+1].draw;
    }
-       player.facing = 'East';
+
 }
 
 function arrows() {
@@ -191,6 +214,42 @@ function check() {
           thingie = 'Your explore the area under your feet and to the ' + player.facing;
           thingie += ', but do not find anything of interest. You note these findings in your journal ';
           thingie += ' and press on.';
+          text.innerHTML = thingie;
+       }
+    }
+  }
+
+function talk() {
+  var thingie = '';
+  if (map[player.mapY][player.mapX].canTalk) {
+    map[player.mapY][player.mapX].talk();
+    return 0;
+  } else {
+      switch(player.facing) {
+        case 'North':
+            if (map[player.mapY-1][player.mapX].canTalk) {
+               map[player.mapY-1][player.mapX].talk();
+               return 0;
+            } 
+        
+        case 'South':
+            if (map[player.mapY+1][player.mapX].canTalk) {
+               map[player.mapY+1][player.mapX].talk();
+               return 0;
+            } 
+
+        case 'West':
+            if (map[player.mapY][player.mapX-1].canTalk) {
+               map[player.mapY][player.mapX-1].talk();
+               return 0;
+            } 
+        case 'East':
+            if (map[player.mapY][player.mapX+1].canTalk) {
+               map[player.mapY][player.mapX+1].talk();
+               return 0;
+            } 
+        default:
+          thingie = 'Talking things out can help you work a problem out.';
           text.innerHTML = thingie;
        }
     }
