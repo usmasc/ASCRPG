125bd3cbe515778a92b5a5eb4e112160ea6b04c4
Scottt <scott.geyer@eagles.usm.edu>
2017-08-08 20:33:50 -0500

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -3,12 +3,38 @@ var sumbit = document.getElementById('submitButtonArea');
 
          // 1     2   3   4   5 6   7   8     9  10   11  12  13  14  15 16 17  18  19 20  21  22   23 24   25  26 27   28 
 
+function up() {
+   if (map[player.mapY-1][player.mapX].canPass){
+     player.mapY--;
+   }
+}
+
+function down() {
+   if (map[player.mapY+1][player.mapX].canPass){
+     player.mapY++;
+   }
+}
+
+
+function left() {
+   if (map[player.mapY][player.mapX-1].canPass){
+     player.mapX--;
+   }
+}
+
+function right() {
+   if (map[player.mapY][player.mapX+1].canPass){
+     player.mapX++;
+   }
+}
+
 function arrows() {
    thingie = '<button onclick="up()">⇑</button><br>';
-    thingie += '<button onclick="left()">⇐</button> ';
-    thingie += '<button onclick="right()">⇒</button><br>';
-    thingie += '<button onclick="down()">⇒</button>';
-    sumbit.innerHTML = thingie;
+   thingie += '<button onclick="left()">⇐</button> ';
+   thingie += '<button onclick="right()">⇒</button><br>';
+   thingie += '<button onclick="down()">⇒</button>';
+   sumbit.innerHTML = thingie;
+}
 
 
 
