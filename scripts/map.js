9930d4cd22b7781600e6e99b15c29763b23bc7a0
Scottt <scott.geyer@eagles.usm.edu>
2017-08-12 12:46:45 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -63,20 +63,43 @@ var wtr = {
 };
 
 var Frd = {
-  'name': 'Fred',
+  'name': 'Fred Paz',
   'canPass': false,
-  'draw': 'floor',
+  'draw': 'Fred',
   'canTalk': true,
   'canCheck': true,
   check() {
-    var thingie = "Fred: Hey, watch where you're touching";
+    var thingie = this.name + ": Hey, watch where you're touching";
     text.innerHTML = thingie;
   },
   talk() {
-    var thingie = "Fred: You found me! What? You never seen an invisible person before?";
+    var thingie = this.name + ": Hello, " + player.name + ". It is time that I tell you a story of grave importance.";
+    thingie += " Care to listen?";
     text.innerHTML = thingie;
+    var buttons = '<button onclick="dialog(1)">Yes</button> <button onclick="dialog(2)">No</button>';
+    sumbit.innerHTML = buttons;
   }
 };
+
+function dialog(n) {
+  var thingie = '';
+  switch(n) {
+    case 1:
+      thingie = 'Fred Paz: ' + player.name + ', our world has been invaded by aliens. They have been here for centuries. ';
+      thingie += 'They have brainwashed much of the populus to believe that they are not capable of doing mathematics. ';
+      thingie += 'We have been fortunate enough to have a segment of the population that has been immune to this sinister ';
+      thingie += 'plot, and we were able to carry on without too many hiccups in the past.';
+      thingie += 'Unfortunately, the aliens have hacked into our cell phone and mobile devices and have been using the ';
+      thingie += 'electronic signals from these devices to convince people that they do not need to know how to do math.;
+      thingie += 'This has created a dire problem for our future. It will not be long before the aliens can take over.';
+      break;
+    case 2:
+      thingie = 'Fred Paz: That is understandable my dear ' + player.name + '. Please return when you are ready.';
+      text.innerHTML = thingie;
+      arrows();
+      break;
+  }
+}
   
 
 var map = {
