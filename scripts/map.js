85b8dc282688f600343adac00434286cab1bf58d
Scottt <scott.geyer@eagles.usm.edu>
2017-08-12 09:51:48 -0500

    Create map.js

---
+++ scripts/map.js
@@ -0,0 +1,101 @@
+var brk = {
+  "canPass": false,
+  "draw": 'brick',
+  'canCheck': false
+};
+
+var rug = {
+  "canPass": true,
+  "draw": 'rug',
+  'canCheck': true,
+  'canTalk': false,
+   check() {
+    var thingie = 'No problems found here. While you are tempted, you refuse to sweep your problems under the rug.';
+    text.innerHTML = thingie;
+  }
+};
+
+var grs = {
+  "canPass": true,
+  "draw": 'grass',
+  'canCheck': true,
+  'canTalk': false,
+  check() {
+    var thingie = 'You could have sworn that the grass looked greener from further away. However, after closer analysis';
+    thingie += ' the grass in the distance looks greener now.';
+    text.innerHTML = thingie;
+  }
+};
+
+var smp = {
+  'canPass':true,
+  'draw':'swamp',
+  'canCheck': false,
+  'canTalk': false
+};
+
+var snd = {
+  'canPass':true,
+  'draw':'sand',
+  'canCheck': false,
+  'canTalk': false
+};
+
+var pth = {
+  'canPass':true,
+  'draw':'path',
+  'canCheck': false,
+  'canTalk': false
+};
+
+var flr = {
+  'canPass':true,
+  'draw':'floor',
+   'canTalk': false,
+  'canCheck': false
+};
+
+var wtr = {
+  'canPass':false,
+  'draw':'water',
+  'canTalk': false,
+  'canCheck': false
+};
+
+var Frd = {
+  'name': 'Fred',
+  'canPass': false,
+  'draw': 'floor',
+  'canTalk': true,
+  'canCheck': true,
+  check() {
+    var thingie = "Fred: Hey, watch where you're touching";
+    text.innerHTML = thingie;
+  },
+  talk() {
+    var thingie = "Fred: You found me! What? You never seen an invisible person before?";
+    text.innerHTML = thingie;
+  }
+};
+  
+
+var map = {
+ 0:[wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr],
+ 1:[wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
+ 2:[wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
+ 3:[wtr,grs,brk,brk,brk,brk,brk,brk,grs,grs,grs,brk,brk,brk,brk,brk,brk,grs,grs,brk,brk,brk,brk,brk,brk,grs,grs,wtr],
+ 4:[wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
+ 5:[wtr,grs,brk,flr,Frd,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
+ 6:[wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
+ 7:[wtr,grs,brk,flr,flr,flr,flr,brk,grs,grs,grs,brk,rug,rug,rug,rug,brk,grs,grs,brk,flr,flr,flr,flr,brk,grs,grs,wtr],
+ 8:[wtr,grs,brk,brk,flr,brk,brk,brk,grs,grs,grs,brk,brk,brk,rug,brk,brk,grs,grs,brk,brk,brk,brk,flr,brk,grs,grs,wtr],
+ 9:[wtr,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,wtr],
+10:[wtr,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,grs,grs,grs,grs,grs,pth,grs,grs,grs,wtr],
+11:[wtr,grs,grs,grs,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,pth,grs,grs,grs,wtr],
+12:[wtr,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,pth,pth,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,grs,wtr],
+13:[wtr,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,wtr],
+14:[wtr,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,snd,wtr],
+15:[wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr],
+16:[wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr,wtr],
+  'rows':17
+};
